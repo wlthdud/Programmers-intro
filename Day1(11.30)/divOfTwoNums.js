017bd6ffdbefb86e66fa8427dad16670b9abbc00
wlthdud <thdudzi@naver.com>
2022-11-30 16:31:05 +0900

    feat: 몫 구하기 문제 풀이 추가

--- Day1(11.30)/divOfTwoNums.js
+++ Day1(11.30)/divOfTwoNums.js
@@ -2,7 +2,7 @@
 //////////
 
 <문제 설명>
-정수 num1, num2가 매개변수 주어집니다. num1과 num2를 곱한 값을 return 하도록 solution 함수를 완성해주세요.
+정수 num1, num2가 매개변수로 주어질 때, num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성해주세요.
 
 <제한사항>
 0 ≤ num1 ≤ 100
@@ -10,10 +10,10 @@
 
 <입출력 예>
 -입출력 예 #1
-num1이 3, num2가 4이므로 3 * 4 = 12를 return합니다.
+num1이 10, num2가 5이므로 10을 5로 나눈 몫 2를 return 합니다.
 
 -입출력 예 #2
-num1이 27, num2가 19이므로 27 * 19 = 513을 return합니다.
+num1이 7, num2가 2이므로 7을 2로 나눈 몫 3을 return 합니다.
 
 //////////
 */
@@ -21,7 +21,7 @@ num1이 27, num2가 19이므로 27 * 19 = 513을 return합니다.
 
 function solution(num1, num2) {
     ExceptionOfNums(num1, num2)
-    var answer = num1 * num2;
+    var answer = parseInt(num1/num2);
     return answer;
 }
 
